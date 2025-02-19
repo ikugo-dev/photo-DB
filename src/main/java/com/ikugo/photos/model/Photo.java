f1b7c46771932597a0e93944d97d732f0ac37e3e
Ikugo <aleksandarristic503@gmail.com>
2025-02-19 22:20:54 +0100

    fully working api!

--- src/main/java/com/ikugo/photos/model/Photo.java
+++ src/main/java/com/ikugo/photos/model/Photo.java
@@ -1,16 +1,27 @@
 package com.ikugo.photos.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.annotations.GenericGenerator;
+import java.util.UUID;
 
+@Entity
+@Table(name = "photos")
 public class Photo {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "BINARY(16)")
+    private UUID id;
     @NotEmpty
     private String fileName;
     @NotEmpty
     private String fileType;
-    @NotEmpty @JsonIgnore
+    @NotEmpty @JsonIgnore @Lob
     private byte[] fileContent;
 
+    public Photo() {}
     public Photo(String fileName, String fileType, byte[] fileContent) {
         this.fileName = fileName;
         this.fileType = fileType;
@@ -35,5 +46,11 @@ public class Photo {
     }
     public void setFileType(String fileType) {
         this.fileType = fileType;
+    }
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
     }
 }
--- src/main/java/com/ikugo/photos/web/PhotoController.java
+++ src/main/java/com/ikugo/photos/web/PhotoController.java
@@ -8,7 +8,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -19,8 +18,8 @@ public class PhotoController {
     }
 
     @GetMapping("/photos")
-    public Map<UUID, Photo> getPhotoDB() {
-        return photoService.getData();
+    public Iterable<Photo> getPhotoDB() {
+        return photoService.getPhotos();
     }
     @GetMapping("/photos/{id}")
     public Photo getPhoto(@PathVariable UUID id) {
@@ -35,20 +34,12 @@ public class PhotoController {
 
     @PostMapping("/photos")
     public void addPhoto(@RequestPart("payload") MultipartFile file) throws IOException {
-        String fileName = file.getOriginalFilename();
-        String fileType = file.getContentType();
-        byte[] fileContent = file.getBytes();
-        Photo photo = new Photo(fileName, fileType, fileContent);
-        photoService.addPhoto(photo);
+        photoService.addPhoto(file);
     }
 
     @DeleteMapping("/photos/{id}")
     public void deletePhoto(@PathVariable UUID id) {
-        Photo photo = photoService.deletePhoto(id);
-        if (photo == null) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    "Photo with id " + id + " not found.");
-        }
+        //doesnt throw ResponseStatusException
+        photoService.deletePhoto(id);
     }
 }
